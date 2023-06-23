758d6d32b031fffa063f099b0843e4db3ac6d65c
Braian-Quintian <bquintianromero@gmail.com>
2023-06-23 13:52:25 -0500

    feat: :sparkles: 01_hello_word

---
+++ 01_hello_word.dart
@@ -0,0 +1,22 @@
+//el main es con lo que comienza
+//siempre una aplicación con dart
+void main(){
+  //var myName = 'Braian'; //esto es un variable
+
+  //String myName = 'Braian';// este es una variable más descriptiva se recomienda para diferenciar el tipo de valor
+
+  //const myName = 'Braian'; //es una constante que nunca va a cambiar de valor
+
+  //final myName = 'Braian'; //es similiar a un const pero menos estricto
+
+  //late final myName; //es un final de inicializacion tardia, No tengo el valor pero se que lo tendré 
+
+  //print('Hola $myName'); //Interpolacion, es concatenar (injectar)
+  //Hola Braian
+  
+  //print('Hola ${ myName.toUpperCase() }'); //Todo lo que se va a ejecutar tiene que estar dentro de {}
+  //hola BRAIAN
+  
+  //print('Hola ${1 + 1}');
+  //hola 2
+}
