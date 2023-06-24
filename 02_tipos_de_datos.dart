81b35cf3a7542dc0d62f1b2d4a1dd94a3d793dc9
Braian-Quintian <bquintianromero@gmail.com>
2023-06-23 19:38:33 -0500

    feat: :sparkles: Update

--- 02_tipos_de_datos.dart
+++ 02_tipos_de_datos.dart
@@ -1,19 +1,33 @@
 void main(){
   
-  final String pokemon = 'Ditto';
-  final int hp = 100;
-  final bool isAlive = true;
-  final List<String> abilities = ['impostor'];
-  final sprites = <String>['ditto/front.png','ditto/back.png'];
+  final String pokemon = 'Ditto'; // una variable final de tipo String
+  final int hp = 100;// una variable final de tipo entero
+  final bool isAlive = true; // una variable de tipo boleano
+  final List<String> abilities = ['impostor']; // una manera de hacer una lista final de tipo string
+  final sprites = <String>['ditto/front.png','ditto/back.png']; // otra manera de hacer una lista de tipo string
   
+// dynamic == null
+  // el dynamic puede ser cualquier tipo de dato,
+  // por esta razon es importante tratar de evitarlo
+  dynamic errorMessage = 'Hola';
+  //errorMessage = true;
+  //errorMessage = [1,2,3,4,5,6]; //lista
+  //errorMessage = { 1,2,3,4,5,6}; //set
+  //errorMessage = () => true; // funcion que retorna true
+  //errorMessage = null;// valor nulo
   
-  
+  //null-safe 
+  // int? a = null Valido null-safe
+  //int? a; El valor inicial de a es NULL
+
+  // para hacer un print de varias lineas solamente es colocar """"
   print("""
   $pokemon
   $hp
   $isAlive
   $abilities
   $sprites
+  $errorMessage
   """);
   
 }
